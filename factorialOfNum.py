a868d29db5051d836c078058a3da86dfc830c1a2
shraddha131004 <155845084+shraddha131004@users.noreply.github.com>
2024-08-16 21:01:39 +0530

    Delete factorialOfNum.py

--- factorialOfNum.py
+++
@@ -1,8 +0,0 @@
-def fact(n):
-    if(n ==1 or n == 0):
-        return 1
-    else:
-        return n*fact(n-1)
-    
-num = int(input("Enter a number "))
-print("Factorial of ",num," is ",fact(num))
